9b60136511d6b706752a74484c99018f07fd7022
Fernando Oyanarte <fernando.oyanarte@gmail.com>
2023-12-06 18:59:15 -0300

    sección preguntas frecuentes

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -2,56 +2,96 @@ import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import './style.css'
 
 function FAQ() {
   return (
     <>
       <Container>
         <Row>
-          <Col sm={12}>
-            <div>
-              <h1>¿En qué podemos ayudarte?</h1>
-              <p>Acá va un párrafo</p>
+          <Col xsm={1} sm={1} md={12} lg={12}>
+            <div className='contenedorEncabezado'>
+              <h1 className='tituloEncabezado'>¿En qué podemos ayudarte?</h1>
+              <p className='parrafoEncabezado'>Acá encontrarás las preguntas más frecuentes. Si tu duda no está resuelta, contactanos. Estaremos encantados de ayudarte.</p>
             </div>
           </Col>          
         </Row>
         <Row>
-          <Col sm={12}>
-            <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
-                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
-                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
-              </svg>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-              </svg>
+          <Col xsm={1} sm={1} md={12} lg={12}>
+            <div className='contenedorContacto '>
+              <div className='contenedorIconoMail'>
+                <img alt='' src='./img/icon_mail_.png' className='iconoFAQ'/>
+                <p>galasbaires@gmail.com</p>
+              </div>
+              <div className='contenedorIconoFacebook'>
+                <img alt='' src='./img/IconoFacebook.png' className='iconoFAQ'/>
+                <p>galasbaires</p>
+              </div>
+              <div className='contenedorIcono'>
+                <img alt='' src='./img/ig.png' className='iconoFAQ'/>
+                <p>@galasbaires</p>
+              </div>
+              <div className='contenedorIcono'>
+                <img alt='' src='./img/Vector.png' className='iconoFAQ'/>
+                <p>San Miguel, Buenos Aires</p>
+              </div>
+              <div className='contenedorIcono'>
+                <img alt='' src='./img/llamada-telefonica.png' className='iconoFAQ'/>
+                <p>11 12345678</p>
+              </div>              
             </div>
           </Col>          
         </Row>
-        <Row>
-          <Col sm={4}></Col>
-          <Col sm={4}>            
-            <Accordion>
-              <Accordion.Item eventKey="0">
+        <Row>          
+          <Col xsm={1} sm={1} md={12} lg={12}>            
+            <Accordion className='acordeonStyle'>
+              <Accordion.Item eventKey="0" className='acordeonItem'>
+                <Accordion.Header>¿Dónde está mi pedido?</Accordion.Header>
+                <Accordion.Body>
+                  Galas Baires te enviará un número de seguimiento del envío, que proporciona el correo, 
+                  para chequear el tracking de tu pedido.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1" className='acordeonItem'>
+                <Accordion.Header>¿Cómo puedo cuidar mi vela?</Accordion.Header>
+                <Accordion.Body>
+                  En el primer encendido de tu Galas Baires no debes apagarla hasta que la cera derretida haya 
+                  alcanzado los bordes del envase.
+                  Recortar la flor que se formó en el pabilo del quemado anterior antes de volver a encenderla.
+                  Si se ensució la superficie de polvo podés limpiarlo con un algodón embebido con alcohol. 
+                  Recordá que tu vela tiene memoria y un mal uso hará que pierda su magia creando un túnel 
+                  en el centro.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2" className='acordeonItem'>
+                <Accordion.Header>Si no sé qué comprar, ¿qué se recomienda?</Accordion.Header>
+                <Accordion.Body>
+                  Escribirnos o realizar el test de posibles elecciones según tus gustos y preferencias. 
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3" className='acordeonItem'>
+                <Accordion.Header>Puntos de retiro</Accordion.Header>
+                <Accordion.Body>
+                  San Miguel, GBA.
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="4" className='acordeonItem'>
                 <Accordion.Header>Políticas de envío</Accordion.Header>
                 <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
+                  <p>
+                    Nuestros envíos a CABA y GBA son realizados por OCA, E-PICK, UBER ENVÍOS. Como también se pueden
+                    coordinar envíos con comisionista de confianza. 
+                  </p> 
+                  <p>Los envíos fuera del área metropolitana se realizarán por VÍA CARGO, CORREO ARGENTINO.</p>
+                  <p>Galas Baires no se responsabiliza si dichos correos entregan los pedidos fuera del plazo acordado. 
+                  Únicamente se garantiza que los envíos serán despachados dentro del plazo acordado con el cliente luego de 
+                  acreditado el pago de los productos.</p>
+                  <p>Los pagos se acreditan de inmediato, aunque si fuese un día viernes, sábado, domingo o días feriados, 
+                  los mismos ingresarán al siguiente día hábil.</p>
                 </Accordion.Body>
               </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Políticas de entrega</Accordion.Header>
+              <Accordion.Item eventKey="5" className='acordeonItem'>
+                <Accordion.Header className='acordeonHeader'>Políticas de entrega</Accordion.Header>
                 <Accordion.Body>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -62,20 +102,8 @@ function FAQ() {
                   culpa qui officia deserunt mollit anim id est laborum.
                 </Accordion.Body>
               </Accordion.Item>
-              <Accordion.Item eventKey="2">
-                <Accordion.Header>Recomendaciones</Accordion.Header>
-                <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="3">
-                <Accordion.Header>Nuestros materiales</Accordion.Header>
+              <Accordion.Item eventKey="6" className='acordeonItem'>
+                <Accordion.Header className='acordeonHeader'>Recomendaciones de uso</Accordion.Header>
                 <Accordion.Body>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                   eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -87,16 +115,10 @@ function FAQ() {
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
-          </Col>
-          <Col sm={4}></Col>
-        </Row>
-        
-      </Container>   
-       
-    </>        
-
-
-    
+          </Col>          
+        </Row>        
+      </Container>          
+    </>            
   );
 }
 
