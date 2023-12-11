d1866c28f3a219c1701d8d70323c95d4dfcdddf1
Fernando Oyanarte <fernando.oyanarte@gmail.com>
2023-12-11 16:29:58 -0300

    modificando sección FAQ

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -89,31 +89,7 @@ function FAQ() {
                   <p>Los pagos se acreditan de inmediato, aunque si fuese un día viernes, sábado, domingo o días feriados, 
                   los mismos ingresarán al siguiente día hábil.</p>
                 </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="5" className='acordeonItem'>
-                <Accordion.Header className='acordeonHeader'>Políticas de entrega</Accordion.Header>
-                <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="6" className='acordeonItem'>
-                <Accordion.Header className='acordeonHeader'>Recomendaciones de uso</Accordion.Header>
-                <Accordion.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in
-                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                  culpa qui officia deserunt mollit anim id est laborum.
-                </Accordion.Body>
-              </Accordion.Item>
+              </Accordion.Item>              
             </Accordion>
           </Col>          
         </Row>        
