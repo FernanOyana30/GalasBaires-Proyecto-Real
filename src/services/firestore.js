d9cc7ec045f4c20e9175db69df39e4cb4a737d3a
Fernando Oyanarte <fernando.oyanarte@gmail.com>
2024-01-20 11:56:38 -0300

    variables de entorno

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import {getFirestore, addDoc, collection} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCpHD-qtUpmMRAD-ki_GFQ8tbECyY4XeE",
-  authDomain: "galas-baires.firebaseapp.com",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: "galas-baires",
   storageBucket: "galas-baires.appspot.com",
-  messagingSenderId: "930077035982",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_ID,
   appId: "1:930077035982:web:86e727538f7ab43c87ef9f"
 };
 
